9fea1692fb5aa97ca1ea238710abf86f64bfff2f
Atul Parihar <atul889dp@gmail.com>
2022-09-12 22:45:44 +0530

    base initial commit

---
+++ features/step-definitions/steps.js
@@ -0,0 +1,34 @@
+import { Given, When, Then } from 'cucumber';
+
+const expectChai = require('chai').expect;
+
+Given(/^I am on the (\w+) page$/, (page) => {
+    let url= `https://the-internet.herokuapp.com/${page}`
+    browser.url(`${url}`);
+});
+
+When(/^I login with (\w+) and (.+)$/, (username, password) => {
+    $('#username').setValue(username);
+    $('#password').setValue(password);
+    $('button[type="submit"]').click(); 
+});
+
+Then(/^I should see a flash message saying (.*)$/, (message) => {
+    expect($('#flash')).toBeExisting();
+    expect($('#flash')).toHaveTextContaining(message);
+    
+    // expect($('#flash').isExisting()).to.be.equals(true); // Chai
+    // expect($('#flash').getText()).contains(message); // Chai
+});
+
+Then(/^I expect url should contain (.*)$/, (uri) => {
+    expect(browser).toHaveUrlContaining(uri)
+
+    // expect(browser).toHaveUrl(uri, { containing: true }) // Matcher Options
+    
+    // expectChai(browser.getUrl()).to.include(uri); // Chai local 
+
+    // expect(browser.getUrl()).to.include(uri); // Chai Global  
+    
+});
+
